1bbc0aa06299b7c40895ecd6f649e755864ffc2c
Santhoshkarthiksk <santhoshkarthik517@gmail.com>
2024-11-09 06:10:09 +0530

    Add files via upload

---
+++ food.js
@@ -0,0 +1,4 @@
+document.getElementById('about-btn').addEventListener('click', function() {
+    const aboutSection = document.getElementById('about-section');
+    aboutSection.classList.toggle('hidden');
+});
